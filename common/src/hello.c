dceab63e63cb7c8b18f8a2b79ed50935f496e6d9
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-10-25 20:53:59 +0200

    init

---
+++ common/src/hello.c
@@ -0,0 +1,6 @@
+#include <game_of_life/hello.h>
+#include <stdio.h>
+
+void hello(const char *name) {
+    printf("Hello, %s!\n", name);
+}
---
+++ serial/src/main.c
@@ -0,0 +1,6 @@
+#include <game_of_life/hello.h>
+
+int main(void) {
+    hello("Serial");
+    return 0;
+}
