01dedaa9bde620edfd9bb72be754cc7237a1666d
bcier <b.ciereszynski@wp.pl>
2024-11-12 18:26:29 +0100

    fix indent

--- blockage/src/worker.c
+++ blockage/src/worker.c
@@ -94,9 +94,8 @@ void worker_run(
         board_get_row(feature_board, 0)
     );
 
-    // evaluate inner rows
     // evaluate inner rows using OpenMP
-#pragma omp parallel for
+    #pragma omp parallel for
     for (int row = 1; row < board->height - 1; row++) {
       const Cell* const previous_row = board_get_row(board, row - 1);
       const Cell* const current_row = board_get_row(board, row);
