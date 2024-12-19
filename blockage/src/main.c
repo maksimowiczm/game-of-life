847f4a8b3e56a7c3d80de0f632a84e3c4fca3f42
bcier <b.ciereszynski@wp.pl>
2024-12-19 20:03:36 +0100

    gather in blockage

--- blockage/src/main.c
+++ blockage/src/main.c
@@ -29,22 +29,17 @@ int main(int argc, char* argv[]) {
   const InitType type = parameters.type;
   const int verbose = parameters.is_verbose;
 
-  if (verbose && processes_count < 3) {
-    fprintf(stderr, "At least 3 processes are required for verbose mode\n");
-    MPI_Finalize();
-    return 1;
-  }
-  if (!verbose && processes_count < 2) {
+  if (processes_count < 2) {
     fprintf(stderr, "At least 2 process is required for non-verbose mode\n");
     MPI_Finalize();
     return 1;
   }
 
-  Board* const board = board_create(height, width);
+  Board* const board = board_create(width, height);
 
   init_board(board, type);
 
-  const int worker_count = verbose ? processes_count - 1 : processes_count;
+  const int worker_count = processes_count;
   int worker_height = height / worker_count;
   int* worker_sizes = malloc(sizeof(int) * worker_count);
   for (int i = 0; i < worker_count; i++) {
@@ -53,67 +48,54 @@ int main(int argc, char* argv[]) {
   // last worker gets the remaining rows
   worker_sizes[worker_count - 1] += (height % worker_count) * width;
 
-  // run manager
   if (verbose && process_id == MANAGER_ID) {
     struct stat st;
-    MPI_Barrier(MPI_COMM_WORLD);
     if (mkdir(output_directory, 0777) == -1 &&
         stat(output_directory, &st) != 0) {
       fprintf(stderr, "Unable to create directory %s\n", output_directory);
-    } else {
-      manager_run(
-          output_directory,
-          processes_count - 1,
-          worker_sizes,
-          board,
-          iterations
-      );
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-    free(worker_sizes);
-    board_destroy(board);
   }
   // run workers
-  else {
-    MPI_Barrier(MPI_COMM_WORLD);
-    double start = MPI_Wtime();
 
-    const int worker_id = verbose ? process_id - 1 : process_id;
-    // last worker gets the remaining rows
-    if (worker_id == worker_count - 1) {
-      worker_height += height % worker_count;
-    }
-
-    // create worker board
-    Board* worker_board = board_create(width, worker_height);
-    worker_get_board_part(
-        board,
-        worker_sizes,
-        worker_id,
-        worker_board->cells,
-        worker_board->width * worker_board->height
-    );
-
-    WorkerType worker_type;
-    if (worker_id == 0) {
-      worker_type = TOP;
-    } else if (worker_id == worker_count - 1) {
-      worker_type = BOTTOM;
-    } else {
-      worker_type = MIDDLE;
-    }
-
-    worker_run(process_id, worker_type, worker_board, iterations, verbose);
-
-    board_destroy(worker_board);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    double stop = MPI_Wtime();
-
-    if (worker_id == 1)
-      printf("%5.4lf\n", stop - start);
+  MPI_Barrier(MPI_COMM_WORLD);
+  double start = MPI_Wtime();
+  const int worker_id = process_id;
+  // last worker gets the remaining rows
+  if (worker_id == worker_count - 1) {
+    worker_height += height % worker_count;
   }
 
+  // create worker board
+  Board* worker_board = board_create(width, worker_height);
+  worker_get_board_part(
+      board,
+      worker_sizes,
+      worker_id,
+      worker_board->cells,
+      worker_board->width * worker_board->height
+  );
+
+  WorkerType worker_type;
+  if (worker_id == 0) {
+    worker_type = TOP;
+  } else if (worker_id == worker_count - 1) {
+    worker_type = BOTTOM;
+  } else {
+    worker_type = MIDDLE;
+  }
+
+  worker_run(process_id, worker_type, worker_board,
+    iterations, verbose, output_directory, worker_sizes);
+
+  board_destroy(worker_board);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double stop = MPI_Wtime();
+
+  if (worker_id == 1)
+    printf("%5.4lf\n", stop - start);
+  free(worker_sizes);
+  board_destroy(board);
   MPI_Finalize();
   return 0;
 }
